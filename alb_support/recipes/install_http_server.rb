425be56776ae7e469d354a931bc5fcfc1d41fa0a
kshitizsekhri <kshitiz.sekhri@twinspark.co>
2019-02-28 21:15:55 +0530

    Update install_http_server.rb

--- alb_support/recipes/install_http_server.rb
+++ alb_support/recipes/install_http_server.rb
@@ -80,9 +80,13 @@ end
 execute "sudo setsebool -P httpd_unified 1" do
   command "sudo setsebool -P httpd_unified 1"
 end
-execute "chcon unconfined_u:object_r:httpd_sys_rw_content_t:s0 webroot/" do
-  command "chcon unconfined_u:object_r:httpd_sys_rw_content_t:s0 webroot/"
-end
+# execute "chcon unconfined_u:object_r:httpd_sys_rw_content_t:s0 webroot/" do
+#   command "chcon unconfined_u:object_r:httpd_sys_rw_content_t:s0 webroot/"
+# end
 execute "sudo vim /etc/httpd/conf/httpd.conf" do
   command "sudo vim /etc/httpd/conf/httpd.conf"
 end
+execute "sed -i '/<Directory "\/var\/www\/html">/,/<\/Directory>/ s/AllowOverride None/AllowOverride all/' /etc/httpd/conf/httpd.conf" do
+  command "sed -i '/<Directory "\/var\/www\/html">/,/<\/Directory>/ s/AllowOverride None/AllowOverride all/' /etc/httpd/conf/httpd.conf"
+end
+
